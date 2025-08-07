1ddb89ddaa3a473d7642c91a380d20d4c28d4055
Ellie Armstrong <ellieearmstrong@gmail.com>
2025-08-07 12:59:16 -0700

    Update pixy.sh

--- 10_diversity/pixy.sh
+++ 10_diversity/pixy.sh
@@ -1,2 +1,3 @@
 ml pixy/1.2.7
-pixy --stats pi dxy fst --vcf allbears.M2.repmap1.indels.DPfilt.QUAL30.drop20miss4dp.AN522.nonref.full.rename.singletoncorr.norel.nocap.gvcf.gz --populations popfile_split2.txt --window_size 50000 --n_cores 24 --output_prefix allbears_allpops_split3_win50kb_pixy
+pixy --stats pi dxy fst --vcf allbears.M2.repmap1.indels.DPfilt.QUAL30.drop20miss4dp.AN522.nonref.full.rename.singletoncorr.norel.nocap.gvcf.gz \
+--populations popfile_split2.txt --window_size 50000 --n_cores 24 --output_prefix allbears_allpops_split3_win50kb_pixy
